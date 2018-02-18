9bbeb601920859b0a54be678a59fe8a93bbed155
MellerB <mellerbartomiej@gmail.com>
2018-02-18 12:45:08 +0100

    updated CSS811 & BME280 example code

---
+++ Sensors/BME280_CCS811.ino
@@ -0,0 +1,71 @@
+#include <SparkFunBME280.h>
+#include <SparkFunCCS811.h>
+
+#define CCS811_ADDR 0x5B 
+#define PIN_NOT_WAKE 5
+
+CCS811 myCCS811(CCS811_ADDR);
+BME280 myBME280;
+
+void setup()
+{
+  Serial.begin(9600);
+  myCCS811.begin();
+  
+  myBME280.settings.commInterface = I2C_MODE;
+  myBME280.settings.I2CAddress = 0x77;
+  myBME280.settings.runMode = 3; 
+  myBME280.settings.tStandby = 0;
+  myBME280.settings.filter = 4;
+  myBME280.settings.tempOverSample = 5;
+  myBME280.settings.pressOverSample = 5;
+  myBME280.settings.humidOverSample = 5;
+
+  delay(5);
+  myBME280.begin();
+
+
+}
+//---------------------------------------------------------------
+void printInfoSerial()
+{
+  Serial.print(" CO2 concentration : ");
+  Serial.print(myCCS811.getCO2());
+  Serial.println(" ppm");
+
+  Serial.print(" TVOC concentration : ");
+  Serial.print(myCCS811.getTVOC());
+  Serial.println(" ppb");
+
+  Serial.print(" Temperature: ");
+  Serial.print(myBME280.readTempC(), 2);
+  Serial.println(" degrees C");
+
+  Serial.print(" Pressure: ");
+  Serial.print(myBME280.readFloatPressure()/100, 2);
+  Serial.println(" hPa");
+
+  Serial.print(" Altitude: ");
+  Serial.print(myBME280.readFloatAltitudeMeters(), 2);
+  Serial.println("m");
+
+  Serial.print(" %RH: ");
+  Serial.print(myBME280.readFloatHumidity(), 2);
+  Serial.println(" %");
+
+  Serial.println();
+  Serial.println("====================================");
+}
+void loop()
+{
+  if (myCCS811.dataAvailable())
+    myCCS811.readAlgorithmResults();
+    printInfoSerial();
+
+    float BMEtempC = myBME280.readTempC();
+    float BMEhumid = myBME280.readFloatHumidity();
+
+    myCCS811.setEnvironmentalData(BMEhumid, BMEtempC);
+  
+  delay(1000);
+}
